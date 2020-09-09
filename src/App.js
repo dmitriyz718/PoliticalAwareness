c75f7e694e4bfe444319cc77f29bc9d536a75e2c
u101004132 <dzag17188@gmail.com>
2020-09-09 13:41:42 -0400

    site done, just style it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import Landing from "./components/Landing";
+import Navigation from "./components/Navigation";
 import Trending from "./components/Trending";
+import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store";
+import Routes from "./config/Routes";
 import { Provider } from "react-redux";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div div className="App">
-          <Landing />
-          <Trending />
-        </div>
+        <Router>
+          <Container>
+            <Navigation />
+            <div div className="App">
+              <Routes />
+              <Trending />
+            </div>
+          </Container>
+        </Router>
       </Provider>
     );
   }
---
+++ src/components/Foreign.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import { connect } from "react-redux";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
+import { fetchNews } from "../actions/News";
+class Foreign extends Component {
+  componentDidMount() {
+    this.props.fetchNews("Foreign");
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.fetchNews(this.state.key);
+  };
+  render() {
+    const newsItems = this.props.news.map((article) => {
+      return (
+        <Row>
+          <Col xs="auto">
+            <Card
+              id={article.id}
+              style={{
+                height: "40%",
+                width: "40%",
+                border: "solid",
+                margin: "2rem",
+              }}
+            >
+              <CardImg
+                top
+                width="100%"
+                src={article.urlToImage}
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle>
+                  <strong>{article.title}</strong>
+                </CardTitle>
+                <CardSubtitle>Athor: {article.author}</CardSubtitle>
+                <CardText>{article.description}</CardText>
+                <Button color="primary" size="lg" active>
+                  View Article
+                </Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      );
+    });
+    return (
+      <Container>
+        <div>
+          <h1>News Dash - Foreign Policy</h1>
+          {newsItems}
+        </div>
+      </Container>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  news: state.news.news,
+});
+export default connect(mapStateToProps, { fetchNews })(Foreign);
